77f5ff73b2ab8242df071ec6ef361ada12fcd4a0
jude <ngwajude 161@gmail.com>
2022-06-03 14:25:04 +0100

    using comments, var declaration, case sensitivity, and using the let keyword

--- jascript.js
+++ jascript.js
@@ -1,2 +1,26 @@
 //judeo
 /** oxox*/
+var myFirstName="Ngwa";
+var myLastName="Jude";
+ 
+//variable assignment
+var a =5;
+var b=10;
+var c="I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//case sensitivity
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+ properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//using let keyword
+let catName = "Oliver";
+let catSound = "Meow!";
