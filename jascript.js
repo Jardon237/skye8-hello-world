7b279fa3430cf7e77ab3975aaaea4431a626f852
jude <ngwajude 161@gmail.com>
2022-06-16 13:03:52 +0100

    Undefined Value returned from a Function

--- jascript.js
+++ jascript.js
@@ -110,3 +110,18 @@ function myOutfit() {
 
 myOutfit();
 
+//Undefined Value returned from a Function
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+function addFive(){
+  sum= sum+5;
+
+}
+
+addThree();
+addFive();
+
